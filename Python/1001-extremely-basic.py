5f99692ad96c0473d5dd0dbd977ddbccd50f7236
GisellyReis <gisellyareis@gmail.com>
2024-06-02 16:11:17 -0300

    create 1001-extremely-basic.py

---
+++ Python/1001-extremely-basic.py
@@ -0,0 +1,16 @@
+# Problem ID: 1001
+# Title: Extremely Basic
+# Link: https://www.beecrowd.com.br/judge/en/problems/view/1001
+
+'''
+Description: 
+Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. 
+Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e 
+não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+'''
+
+a = int(input())
+b = int(input())
+
+x = a + b
+print("X = %d" % x)
