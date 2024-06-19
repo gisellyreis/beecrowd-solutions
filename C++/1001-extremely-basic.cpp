5a283d2ef0a15a31f205e2f8d8e5b8b95cb244e7
GisellyReis <gisellyareis@gmail.com>
2024-06-18 21:47:21 -0300

    create 1001-extremely-basic.cpp

---
+++ C++/1001-extremely-basic.cpp
@@ -0,0 +1,25 @@
+
+# Problem ID: 1001
+# Title: Extremely Basic
+# Link: https://www.beecrowd.com.br/judge/en/problems/view/1001
+
+'''
+Description: 
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. 
+Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+'''
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  
+  int a, b, x=0;
+  cin >> a >> b;
+  x = a + b;
+  cout << "X = " << x << endl;
+
+
+  return 0;
+}
